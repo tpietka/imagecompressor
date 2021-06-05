0f8ff178bba6e077b1dcb709ae0dfbc2fadd445f
tpietka <tomaszpietka92@gmail.com>
2021-06-05 20:57:19 +0200

    initial commit

---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Main",
+  props: {
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
