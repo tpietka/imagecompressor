f85de4f12ef28c994e4e537d1bd34f2ff13ab2bb
tpietka <tomaszpietka92@gmail.com>
2021-06-07 21:23:59 +0200

    Add images comparssion lib and slider

--- src/components/Compare.vue
+++ src/components/Compare.vue
@@ -11,6 +11,7 @@
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
 import { ImageFile } from "../types";
+import ImageCompare from "image-compare-viewer";
 
 export default defineComponent({
   name: "Compare",
@@ -32,8 +33,50 @@ export default defineComponent({
       return "";
     },
   },
+  mounted() {
+    const element = document.getElementById("image-compare");
+    const options = {
+      showLabels: true,
+      labelOptions: {
+        before: this.$parent.prettySize(this.image.file.size),
+        after: this.$parent.prettySize(this.image.compressedFile.size),
+        onHover: false,
+      },
+    };
+    new ImageCompare(element, options).mount();
+  },
 });
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.modal {
+  position: fixed;
+  z-index: 1120;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-width: 100%;
+  max-height: 100%;
+  width: auto;
+}
+.modal-backdrop {
+  z-index: 1090;
+  background: rgba(0, 0, 0, 0.75);
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+}
+.img-left {
+  width: 50%;
+  height: auto;
+  object-fit: cover;
+}
+.img-right {
+  width: 50%;
+  height: auto;
+  object-fit: cover;
+}
+</style>
