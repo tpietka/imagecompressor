d81b2c7b87e0b62531b718b44a24bd58f8520179
tpietka <tomaszpietka92@gmail.com>
2021-07-02 00:17:10 +0200

    add other compressionOptions types

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,23 @@ export interface ImageFile {
   file: File | Blob;
   savedOnCompression: string;
 }
+
+export interface Watermark {
+  size: string;
+  text: string;
+  font: string;
+}
+
+export interface CompressorOptions {
+  quality: string;
+  minWidth: string | number | string[] | undefined;
+  maxWidth: string | number | string[] | undefined;
+  minHeight: string | number | string[] | undefined;
+  maxHeight: string | number | string[] | undefined;
+  width: string | number | string[] | undefined;
+  height: string | number | string[] | undefined;
+  mimeType: string | number | string[] | undefined;
+  convertSize: string | number | string[] | undefined;
+  strict: boolean;
+  checkOrientation: boolean;
+}
