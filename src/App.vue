c748d55a719f10c557886944ab5086c3afabb79f
tpietka <tomaszpietka92@gmail.com>
2022-03-16 23:11:27 +0100

    Change component to script setup

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,8 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
 import Main from "./components/Main.vue";
-
-export default defineComponent({
-  name: "App",
-  components: {
-    Main,
-  },
-});
 </script>
 
 <style lang="scss">
